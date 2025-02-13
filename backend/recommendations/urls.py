8549de9dfdbf0565dac8ac5f5696db34cb599c67
Michael Joseph <99811467+Mikiejoe@users.noreply.github.com>
2025-02-13 04:54:38 +0300

    Update urls.py

--- backend/recommendations/urls.py
+++ backend/recommendations/urls.py
@@ -12,7 +12,5 @@ urlpatterns = [
     path("chat-lessons/", views.chat_lessons, name="chat-lessons"),
     path("student-chat/", views.student_chat, name="student-chat"),
     path("career-chat/", views.careerchat, name="career-chat"),
-    path("", include(router.urls)),
-]
-
-# router.urls
+    # path("", include(router.urls)),
+]+router.urls
