351e528c081cdc78d3123092ab860b95084eda4e
Michael Joseph <99811467+Mikiejoe@users.noreply.github.com>
2025-02-13 04:50:41 +0300

    Update urls.py

--- backend/elimika/urls.py
+++ backend/elimika/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('api/v1/auth/', include('dj_rest_auth.urls')),
     path('api/v1/auth/register/', include("dj_rest_auth.registration.urls")),
     path('api/v1/', include('school.urls')),
-    path('api/v1//recommendations/', include('recommendations.urls')),
+    path('api/v1/recommendations/', include('recommendations.urls')),
 ]
